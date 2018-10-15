3c51104946221adb2fa71ebd0b05bae9194713ec
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-10-14 20:23:23 -0500

    an page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>hello</h1>
-      </div>
+      <BrowserRouter >
+        <div className="body">
+          
+            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+            
+        </div>
+      </BrowserRouter>
     );
   }
 }
